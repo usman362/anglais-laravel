206347724c18b4fb62ca4bcc340a5375d06d75f6
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-05-16 04:46:17 +0500

    cah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,29 @@
         <div id="google_translate_element"></div>
 
         <script type="text/javascript">
-        function googleTranslateElementInit() {
-        new google.translate.TranslateElement({pageLanguage: 'en',includedLanguages:'fr'}, 'google_translate_element');
-        }
+       function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en',
+            includedLanguages: 'fr',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+    }
+
+    // Wait for the widget to be ready, then auto-select 'fr'
+    function autoSelectFrench() {
+        const interval = setInterval(function () {
+            const select = document.querySelector('.goog-te-combo');
+            if (select) {
+                select.value = 'fr';
+                select.dispatchEvent(new Event('change')); // trigger change
+                clearInterval(interval);
+            }
+        }, 500);
+    }
+
+    window.addEventListener('load', function () {
+        autoSelectFrench();
+    });
         </script>
 
         <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
