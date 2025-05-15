0ce00989c2c930cba8ba776414f5fc788433f08c
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-05-16 04:39:53 +0500

    channges

--- resources/views/translate.blade.php
+++ resources/views/translate.blade.php
@@ -17,6 +17,26 @@
                 includedLanguages: 'fr'
             }, 'google_translate_element');
         }
+
+// Auto-translate to French on load
+function setFrenchLanguage() {
+    var cookieName = "googtrans";
+    var cookieValue = "/en/fr";
+    var domain = window.location.hostname;
+
+    document.cookie = cookieName + "=" + cookieValue + ";path=/;domain=" + domain;
+
+    // Force URL hash for Google Translate
+    if (window.location.hash !== "#googtrans(/en/fr)") {
+      window.location.hash = "#googtrans(/en/fr)";
+      window.location.reload();
+    }
+  }
+
+  // Trigger on page load
+  window.onload = function () {
+    setFrenchLanguage();
+  };
 </script>
 
 <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
