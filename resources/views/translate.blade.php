ab93d058781fc387a3a4bb8d3da6ba0ddf41a7c0
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-05-16 04:36:22 +0500

    changes

--- resources/views/translate.blade.php
+++ resources/views/translate.blade.php
@@ -11,9 +11,12 @@
 <div id="google_translate_element"></div>
 
 <script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-}
+ function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                includedLanguages: 'fr'
+            }, 'google_translate_element');
+        }
 </script>
 
 <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
