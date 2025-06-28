d77b4fc74e43be51737e75ee7c94856dc49c9f19
Muhammad Usman <120487584+alidevelopement012@users.noreply.github.com>
2025-06-28 19:01:12 +0500

    changes

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,8 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>
+        {{-- @yield('title') --}}
+        Aide à domicile - Agapé Nantes
+    </title>
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/images/SMILEY-AGAPEÌ_-2.png') }}">
+    <meta name="title" >
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/plugins/swiper.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/plugins/fontawesome-5.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/plugins/animate.min.css') }}">
